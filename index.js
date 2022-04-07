44863c2a49087a90d6a4cb52fe99b6152a0004f5
josephgeorge144 <josephgeogre144@gmial.com>
2022-04-07 17:05:17 +0100

    header styles added using dom and css, new html elements added

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-console.log("hello world")
-console.log("hello world")
+nodeList=document.querySelectorAll(".fixed-nav ul a li")
+for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].addEventListener('mouseover',mouse);
+    nodeList[i].addEventListener('mouseout',mouseout)
+}
+
+
+
+function mouse(event){
+   
+
+    event.target.style.borderBottom = "thick solid rgb(260, 217, 2)";
+    event.target.style.transition = "all .09s ease-in-out"
+
+
+ 
+    
+
+
+
+
+   
+
+}
+function mouseout(event){
+    event.target.style.borderBottom = "none";
+    event.target.style.transition = "all .4s ease-out-in"
+  
+}
+
+
+
+
